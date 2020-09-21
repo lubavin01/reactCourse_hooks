76668fe404e7d58e8c6195e57af3287ab0f0763f
Любавин Николай Андреевич <nikolay.lubavin@coolclever.ru>
2020-09-21 10:30:45 +0300

    useEffect 100

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const App = () => {
   const [visible, setVisible] = useState(true);
 
   if (visible) {
+
+    // setTimeout(() => setVisible(false), 1500);
+
     return (
       <div>
         <button onClick={() => setValue(v => v + 1)}>+</button>
         <button onClick={() => setVisible(false)}>hide</button>
-        <ClassCounter value={value} />
-        <HookCounter value={value} />
+        {/* <ClassCounter value={value} /> */}
+        {/* <HookCounter value={value} /> */}
+        <Notification />
       </div >
     );
   } else {
@@ -21,12 +25,35 @@ const App = () => {
 
 const HookCounter = ({ value }) => {
 
+  const [mounted, setMounted] = useState(false);
+
   useEffect(() => {
-    console.log('use effect');
-    return () => console.log('clear')
-  }, null);
+    console.log('mount');
+    setMounted(true);
+
+    return () => console.log('unmount');
+  }, []);
+
+  useEffect(() => {
+    if (!mounted) {
+      return;
+    }
+    console.log('update')
+  }, [value]);
 
   return (<p>{value}</p>)
+}
+
+const Notification = () => {
+
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setVisible(false), 2000);
+    return () => clearTimeout(timeout)
+  }, []);
+
+  return <div>{visible && 'Hello'}</div>
 }
 
 class ClassCounter extends Component {
