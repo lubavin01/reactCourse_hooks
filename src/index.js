8c22767ac16a5b7a56e159172cd3853de0592ca3
Любавин Николай Андреевич <nikolay.lubavin@coolclever.ru>
2020-09-22 17:58:15 +0300

    101 use effect

--- src/index.js
+++ src/index.js
@@ -2,20 +2,15 @@ import React, { Component, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(1);
   const [visible, setVisible] = useState(true);
 
   if (visible) {
-
-    // setTimeout(() => setVisible(false), 1500);
-
     return (
       <div>
         <button onClick={() => setValue(v => v + 1)}>+</button>
         <button onClick={() => setVisible(false)}>hide</button>
-        {/* <ClassCounter value={value} /> */}
-        {/* <HookCounter value={value} /> */}
-        <Notification />
+        <PlanetInfo id={value} />
       </div >
     );
   } else {
@@ -23,55 +18,21 @@ const App = () => {
   }
 }
 
-const HookCounter = ({ value }) => {
-
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    console.log('mount');
-    setMounted(true);
-
-    return () => console.log('unmount');
-  }, []);
+const PlanetInfo = ({ id }) => {
+  
+  const [name, setName] = useState('undef');
 
   useEffect(() => {
-    if (!mounted) {
-      return;
-    }
-    console.log('update')
-  }, [value]);
+    fetch(`https://swapi.dev/api/planets/${id}`, { method: 'GET' })
+      .then(data => data.json())
+      .then(dataParsed => {
+         setName(dataParsed.name);
+      })
+      .catch(err => console.log('ERRROR:', err));
+  }, [id]);
 
-  return (<p>{value}</p>)
-}
 
-const Notification = () => {
-
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => setVisible(false), 2000);
-    return () => clearTimeout(timeout)
-  }, []);
-
-  return <div>{visible && 'Hello'}</div>
-}
-
-class ClassCounter extends Component {
-  componentDidMount() {
-    console.log('component did mount');
-  }
-
-  componentDidUpdate() {
-    console.log('component did update');
-  }
-
-  componentWillUnmount() {
-    console.log('component will unmount');
-  }
-
-  render() {
-    return <p>{this.props.value}</p>
-  }
+  return (<div>{`${id} - ${name}`}</div>);
 }
 
 ReactDOM.render(
